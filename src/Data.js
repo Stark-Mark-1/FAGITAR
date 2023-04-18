9b48444acec9e9012b3f27c1f4a0d499fa31c602
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-04-18 22:16:16 +0530

    navbar with animation

---
+++ src/Data.js
@@ -0,0 +1,26 @@
+export const links=[
+    {
+        name:"Home",
+        path:'/'
+    },
+    {
+        name:"About",
+        path:'/about'
+    },
+    {
+        name:"Blog",
+        path:'/blog'
+    },
+    {
+        name:"FagitAR",
+        path:'/fagitAR'
+    },
+    {
+        name:"Pricing",
+        path:'/pricing'
+    },
+    {
+        name:"Contact Us",
+        path:'/contact'
+    }
+];
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import Home from './home';
-import FagitarAR from './Fagitar-AR';
-import Pricing from './pricing';
-import Blogs from './blogs';
-import About from './about';
-const navbar = () => {
+import './navbarStyle.css'
+import {useState} from 'react';
+import { Link, NavLink } from 'react-router-dom'
+import { links } from '../Data'
+import Logo from '../Images/logo.png'
+import { GoThreeBars } from 'react-icons/go'
+import {MdOutlineClose} from 'react-icons/md';
+const Navbar = () => {
+const[showMenu,setMenu]=useState(false);
+
   return (
-    <div>
-      <Home/>
-      <Pricing/>
-    </div>
+    <nav>
+      <div className="container nav__container">
+        <Link to='/' className='Logo'>
+          <img src={Logo} alt="App logo" />
+        </Link>
+        <ul className={`nav__links ${showMenu ? 'show-menu':'hide-menu'}`}>
+          {
+            links.map(({ name, path }, index) => {
+              return (
+                <li key={index}>
+                  <NavLink to={path} className={({ isActive }) => isActive ? 'active-nav' : ''}>{name}</NavLink>
+                </li>)
+            })
+          }
+        </ul>
+        <button className='nav_toggle-btn' onClick={()=>setMenu(!showMenu)}>
+          {
+            showMenu?<MdOutlineClose/>:<GoThreeBars/>
+          }
+        </button>
+      </div>
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar
