2659977a03a509dd5dae0ed14ae33fa6546859e3
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-04-23 20:20:34 +0530

    on click close navbar option added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,35 +2,35 @@ import './navbarStyle.css'
 import {useState} from 'react';
 import { Link, NavLink } from 'react-router-dom'
 import { links } from '../Data'
-import Logo from '../Images/logo.png'
+import Logo from '../Images/LOGO.png'
 import { GoThreeBars } from 'react-icons/go'
 import {MdOutlineClose} from 'react-icons/md';
 const Navbar = () => {
 const[showMenu,setMenu]=useState(false);
 
   return (
-    <nav>
+    <div className='nav'>
       <div className="container nav__container">
-        <Link to='/' className='Logo'>
-          <img src={Logo} alt="App logo" />
+        <Link to='/' className='Logo' onClick={() => setMenu(false)}>
+          <img src={Logo} alt="App logo" className='LoGo' />
         </Link>
         <ul className={`nav__links ${showMenu ? 'show-menu':'hide-menu'}`}>
           {
             links.map(({ name, path }, index) => {
               return (
                 <li key={index}>
-                  <NavLink to={path} className={({ isActive }) => isActive ? 'active-nav' : ''}>{name}</NavLink>
+                  <NavLink to={path} className={({ isActive }) => isActive ? 'active-nav' : ''} onClick={()=>setMenu(prev => !prev)}>{name}</NavLink>
                 </li>)
             })
           }
         </ul>
-        <button className='nav_toggle-btn' onClick={()=>setMenu(!showMenu)}>
+        <button className='nav_toggle-btn' onClick={()=>setMenu(prev => !prev)}>
           {
             showMenu?<MdOutlineClose/>:<GoThreeBars/>
           }
         </button>
       </div>
-    </nav>
+    </div>
   )
 }
 
