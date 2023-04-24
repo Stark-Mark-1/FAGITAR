a335d6ad7f4a77b4a566d571b53ddcd43eef301e
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-04-24 07:44:47 +0530

    programs data added

--- src/Data.js
+++ src/Data.js
@@ -1,3 +1,4 @@
+import {TbBrandMiniprogram} from 'react-icons/tb'
 export const links=[
     {
         name:"Home",
@@ -24,3 +25,33 @@ export const links=[
         path:'/contact'
     }
 ];
+export const programs=[
+    {
+        id:1,
+        icon:<TbBrandMiniprogram/>,
+        title:"program one",
+        info:"This is a sample card layout",
+        path:"/programs/111",
+    },
+    {
+        id:2,
+        icon:<TbBrandMiniprogram/>,
+        title:"program two",
+        info:"This is a sample card layout",
+        path:"/programs/222",
+    },
+    {
+        id:3,
+        icon:<TbBrandMiniprogram/>,
+        title:"program three",
+        info:"This is a sample card layout",
+        path:"/programs/333",
+    },
+    {
+        id:4,
+        icon:<TbBrandMiniprogram/>,
+        title:"program four",
+        info:"This is a sample card layout",
+        path:"/programs/444",
+    },
+]
