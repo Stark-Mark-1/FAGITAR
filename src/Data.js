174cce6d3d1a186dd00528ca8fba712c0244ede7
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-05-07 10:38:36 +0530

    Data for testimonial, plans, faq and programs inserted

--- src/Data.js
+++ src/Data.js
@@ -1,4 +1,5 @@
 import {TbBrandMiniprogram} from 'react-icons/tb'
+import require from 'react'
 export const links=[
     {
         name:"Home",
@@ -53,5 +54,128 @@ export const programs=[
         title:"program four",
         info:"This is a sample card layout",
         path:"/programs/444",
+    },
+]
+export const faqs=[
+    {
+        id:1,
+        question:"how FagitAR made ?",
+        answer:"Dolor delectus natus id, voluptatibus tenetur esse laborum! Neque officia excepturi illo laborum. Magnam, debitis fugit. Facere dignissimos soluta labore debitis architecto?" 
+    },
+    {
+        id:2,
+        question:"how to register ?",
+        answer:"Dolor delectus natus id, voluptatibus tenetur esse laborum! Neque officia excepturi illo laborum. Magnam, debitis fugit. Facere dignissimos soluta labore debitis architecto?"
+    },
+    {
+        id:3,
+        question:"What is plan for this ?",
+        answer:"Dolor delectus natus id, voluptatibus tenetur esse laborum! Neque officia excepturi illo laborum. Magnam, debitis fugit. Facere dignissimos soluta labore debitis architecto?"
+
+    },
+    {
+        id:4,
+        question:"About FagitAR ?",
+        answer:"Dolor delectus natus id, voluptatibus tenetur esse laborum! Neque officia excepturi illo laborum. Magnam, debitis fugit. Facere dignissimos soluta labore debitis architecto?"
+
+    }
+]
+
+
+
+
+export const testimonials=[
+    {
+        id:1,
+        name:"Alan",
+        quote:"amet consectetur adipisicing elit. Exercitationem, expedita sunt voluptatum fuga eligendi ullam quae in perspiciatis illum a! Excepturi sequi quas nam. Adipisci ab quibusdam voluptatibus fugit reprehenderit?",
+        job:"Doctor",
+        avatar:require("./Images/1.jpeg")
+    },
+    {
+        id:2,
+        name:"Elon",
+        quote:"amet consectetur adipisicing elit. Exercitationem, expedita sunt voluptatum fuga eligendi ullam quae in perspiciatis illum a! Excepturi sequi quas nam. Adipisci ab quibusdam voluptatibus fugit reprehenderit?",
+        job:"student",
+        avatar:require("./Images/2.jpeg")
+    },
+    {
+        id:3,
+        name:"Jorge",
+        quote:"amet consectetur adipisicing elit. Exercitationem, expedita sunt voluptatum fuga eligendi ullam quae in perspiciatis illum a! Excepturi sequi quas nam. Adipisci ab quibusdam voluptatibus fugit reprehenderit?",
+        job:"Engineer",
+        avatar:require("./Images/3.jpeg")
+    },
+    {
+        id:4,
+        name:"Bassanio",
+        quote:"amet consectetur adipisicing elit. Exercitationem, expedita sunt voluptatum fuga eligendi ullam quae in perspiciatis illum a! Excepturi sequi quas nam. Adipisci ab quibusdam voluptatibus fugit reprehenderit?",
+        job:"Student",
+        avatar:require("./Images/4.jpeg")
+    },
+    {
+        id:5,
+        name:"David",
+        quote:"amet consectetur adipisicing elit. Exercitationem, expedita sunt voluptatum fuga eligendi ullam quae in perspiciatis illum a! Excepturi sequi quas nam. Adipisci ab quibusdam voluptatibus fugit reprehenderit?",
+        job:"Engineer",
+        avatar:require("./Images/5.jpeg")
+    }
+]
+
+
+export const plans=[
+    {
+        id:1,
+        name:'Silver Package',
+        desc:"This package is perfect for small Buisness Venture Capital",
+        price:1000,
+        features:[
+            {feature:'First Feature',available:true},
+            {feature:'second Feature',available:true},
+            {feature:'Third Feature',available:true},
+            {feature:'Fourth Feature',available:true},
+            {feature:'Fifth Feature',available:false},
+            {feature:'Sixth Feature',available:false},
+            {feature:'Seventh Feature',available:false},
+            {feature:'Eighth Feature',available:false},
+            {feature:'Ninth Feature',available:false},
+            {feature:'Tenth Feature',available:false}
+        ]
+    },
+    {
+        id:2,
+        name:'Gold Package',
+        desc:"This package is for those who want expand their buisness",
+        price:3000,
+        features:[
+            {feature:'First Feature',available:true},
+            {feature:'second Feature',available:true},
+            {feature:'Third Feature',available:true},
+            {feature:'Fourth Feature',available:true},
+            {feature:'Fifth Feature',available:true},
+            {feature:'Sixth Feature',available:true},
+            {feature:'Seventh Feature',available:true},
+            {feature:'Eighth Feature',available:false},
+            {feature:'Ninth Feature',available:false},
+            {feature:'Tenth Feature',available:false}
+        ]
+    },
+    {
+        id:3,
+        name:'Platinum Package',
+        desc:"This package is for those who want try new things",
+        price:7000,
+        features:[
+            {feature:'First Feature',available:true},
+            {feature:'second Feature',available:true},
+            {feature:'Third Feature',available:true},
+            {feature:'Fourth Feature',available:true},
+            {feature:'Fifth Feature',available:true},
+            {feature:'Sixth Feature',available:true},
+            {feature:'Seventh Feature',available:true},
+            {feature:'Eighth Feature',available:true},
+            {feature:'Ninth Feature',available:true},
+            {feature:'Tenth Feature',available:true}
+        ]
     },
 ]
