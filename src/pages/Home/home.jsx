1bd25c17bf854cadac5045e2d49dfe9e8f608012
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-05-07 10:23:54 +0530

    Home section with footer and other components

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,17 @@
 import './homeStyle.css'
 import Header from '../../components/MainHeader';
 import Program from '../../components/Programs';
+import Faq from '../../components/FAQs';
+import Testimonial from '../../components/Testimonial';
+import Footer from '../../components/Footer';
 const home = () => {
   return (
     <>
     <Header/>
     <Program/>
+    <Faq/>
+    <Testimonial/>
+    <Footer/>
     </>
   )
 }
